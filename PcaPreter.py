c95007a438bfcf77cad86584dffa26acee09cd8f
Default37 <fernando_souza_vasconcelos@hotmail.com>
2020-12-06 13:59:49 -0300

    http_analysis 1.0

--- PcaPreter.py
+++ PcaPreter.py
@@ -23,7 +23,8 @@ def main():
         print('2 - All Packet Content')
         print('3 - All TCP Packets')
         print('4 - All UDP Packets')
-        print('5 - Quit')
+        print('5 - HTTP Analysis')
+        print('0 - to Break')
         menu = int(input('Selection: '))
         if(menu == 1):
             espec_packet(packet_list)
@@ -39,6 +40,8 @@ def main():
         elif(menu == 4):
             udp_packet(packet_list)
         elif(menu == 5):
+            http_analysis(packet_list)
+        else:
             break
     
 def espec_packet(packet_list):
@@ -53,12 +56,62 @@ def full_packet(packet_list):
 def tcp_packet(packet_list):
     for i in range(len(packet_list)):
         if packet_list[i].haslayer(TCP):
-            if packet_list[i].dport == 58436:
-                packet_list[i].show()
+            packet_list[i].show()
 
 def udp_packet(packet_list):
     for i in range(len(packet_list)):
         if packet_list[i].haslayer(UDP):
             packet_list[i].show()
+
+def http_analysis(packet_list):
+    rcv = 0
+    snt = 0
+    https_sent = 0
+    http_sent = 0
+    ftp_sent = 0
+    https_rcv = 0
+    http_rcv = 0
+    ftp_rcv = 0
+    for i in range(len(packet_list)):
+        if packet_list[i].haslayer(TCP):
+            if ((packet_list[i].dport == 'https') or (packet_list[i].dport == 443)):
+                snt += 1
+                https_sent += 1
+            elif ((packet_list[i].dport == 80) or (packet_list[i].dport == 8080)):
+                snt += 1
+                http_sent += 1
+            elif ((packet_list[i].dport == 21)):
+                snt += 1
+                ftp_sent += 1
+            if ((packet_list[i].sport == 'https') or (packet_list[i].sport == 443)):
+                rcv += 1
+                https_rcv += 1
+            elif ((packet_list[i].sport == 80) or (packet_list[i].sport == 8080)):
+                rcv += 1
+                http_rcv += 1
+            elif ((packet_list[i].sport == 21)):
+                rcv += 1
+                ftp_rcv += 1
+    if (rcv > snt):
+        print('-------------------------------------------------------------------')
+        print('[*] There was %d packets sent!' %snt)
+        print('[*] There was %i packets received!' %rcv)
+        print('[*] This User is a Mostly Downloader!!!')
+        print('-------------------------------------------------------------------')
+        print('[*] There was %i https packets sent and %i received' %(https_sent, https_rcv))
+        print('[*] There was %i http packets sent and %i received' %(http_sent, http_rcv))
+        print('[*] There was %i ftp packets sent and %i received' %(ftp_sent, ftp_rcv))
+        print('-------------------------------------------------------------------')
+    else:
+        print('-------------------------------------------------------------------')
+        print('[*] There was %d packets sent!' %snt)
+        print('[*] There was %i packets received!' %rcv)
+        print('[*] This User is a Mostly Uploader!!!')
+        print('-------------------------------------------------------------------')
+        print('[*] There was %i https packets sent and %i received' %(https_sent, https_rcv))
+        print('[*] There was %i http packets sent and %i received' %(http_sent, http_rcv))
+        print('[*] There was %i ftp packets sent and %i received' %(ftp_sent, ftp_rcv))
+        print('-------------------------------------------------------------------')
+            
 
 main()
